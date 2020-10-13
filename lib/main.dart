6a1208b85f886966fbe65f14436ec413bdefab73
captnseagraves <k.s.seagraves@gmail.com>
2020-10-13 13:17:25 -0700

    completed on desktop, iOS seems to have a black background with black text which makes only the lower icons appear

--- lib/main.dart
+++ lib/main.dart
@@ -28,25 +28,83 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyBusinessCard(),
+      home: MyBusinessCard(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyBusinessCard extends StatelessWidget {
+  MyBusinessCard({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Text(
-          'Kevin Seagraves',
-          style: Theme.of(context).textTheme.headline5,
-        ),
-        Text(
-          'Experienced Flutter Developer',
-          style: Theme.of(context).textTheme.headline5,
+        Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.account_circle, size: 50),
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Kevin Seagraves',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                Text(
+                  'Experienced Flutter Developer',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '123 Main Street',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    Text(
+                      '(415) 555-0198',
+                      style: Theme.of(context).textTheme.headline6,
+                    )
+                  ],
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.accessibility,
+                      size: 50,
+                      color: Colors.blue,
+                    ),
+                    Icon(
+                      Icons.timer,
+                      size: 50,
+                      color: Colors.blue,
+                    ),
+                    Icon(
+                      Icons.phone_android,
+                      size: 50,
+                      color: Colors.blue,
+                    ),
+                    Icon(
+                      Icons.phone_iphone,
+                      size: 50,
+                      color: Colors.blue,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ],
     );
